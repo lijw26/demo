f0537f5834a46ac2bc2a857385b99bd31cb2163c
lijw26 <864630776@qq.com>
2021-03-30 09:56:08 +0800

    Array

---
+++ src/Array012.java
@@ -0,0 +1,10 @@
+public class Array012 {
+
+    public  static void printArray(int[] array){
+        printArray(new int[]{ 1,2,3,4 });
+        for (int i = 0 ; i < array.length ; i++){
+            System.out.println(array[i]);
+        }
+
+    }
+}
---
+++ src/Test07for.java
@@ -0,0 +1,8 @@
+public class Test07for {
+    public static void main(String[] args){
+
+        for (int x=1;x>0&x<20;x++){
+            System.out.println("Counting x to "+x);
+        }
+    }
+}
