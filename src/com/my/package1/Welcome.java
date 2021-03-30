2bbdd2e8e0ba7c93cc80c844cce5eed0006e04c2
lijw26 <864630776@qq.com>
2021-03-30 09:54:17 +0800

    Revert "ArrayLearning"
    This reverts commit 63efddd5b75da3c1a4defae7a88b58e73ee4b45b.

--- src/com/my/package1/Welcome.java
+++
@@ -1,9 +0,0 @@
-public class Welcome {
-    public static void main(String[] args){
-        String greeting = "Welcome to Core Java!";
-        System.out.println(greeting);
-        for (int i = 0;i < greeting.length(); i++)//控制下行符号长度,i=0跟greeting等长
-            System.out.print("-");
-        System.out.println();
-    }
-}
